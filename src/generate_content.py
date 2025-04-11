c0416230253e90cb95669f849bef83362058a40e
Anatolii Sapozhnikov <asap.web.dev.projects@gmail.com>
2025-04-12 00:00:35 +0200

    static site generator

--- src/generate_content.py
+++ src/generate_content.py
@@ -22,17 +22,17 @@ def extract_title(md):
             return line[2:]
     raise ValueError("no title found")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         if os.path.isfile(from_path):
             dest_path = os.path.join(dest_dir_path, "index.html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
             dest_path = os.path.join(dest_dir_path, filename)
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -48,6 +48,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import sys
 import os
 import shutil
 
 from generate_content import copy_files_recursive, generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -21,6 +24,7 @@ def main():
         dir_path_content,
         template_path,
         dir_path_public,
+        basepath
     )
 
 main()
